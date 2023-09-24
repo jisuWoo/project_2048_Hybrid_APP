450cf8018220e93c5dca3c2ef5c3e2e9798d2b36
jisuWoo <127714602+jisuWoo@users.noreply.github.com>
2023-09-24 22:20:43 +0900

    style update
    간단한 스타일을 업데이트

--- 01_html/event.js
+++ 01_html/event.js
@@ -8,7 +8,7 @@ window.onload = function(){
             loginbtn.style.display = "none";
             logoutbtn.style.display = "block";
 
-            result.innerText = "다임";
+            result_txt.innerText = "이은석";
             var img = document.createElement("img");
             img.src = "https://vr.yuhan.ac.kr/bbs/file/getImageDirect.do?subFilePath=professor&fileName=professor_file_1_49039211373641942.JPG";
             result.appendChild(img);
@@ -20,8 +20,8 @@ window.onload = function(){
         result.innerText = "";
     });
 }
-var arr = ["자기소개 : 안녕하세요, 이은석 교수님이지요. 게임 공학 분야의 교육자로, 혁신적이고 독특한 교육 방식을 통해 학생들을 인도합니다.게임 개발의 비밀과 아름다움을 탐험하고, 학생들이 창의적으로 문제를 해결하며 능력을 키우도록 격려합니다. 협업과 팀워크를 강조하며, 학생들과 함께 게임 개발의 새로운 세계를 탐험합니다. 출처 : chatgpt",
-"이은석교수님에 대한 소감: 이은석 교수님의 강의와 지도는 마치 마법처럼 환상적입니다. 그의 독특한 교육 방식은 학생들에게 현실적인 경험을 제공하고, 게임 개발의 실무 스킬을 강화합니다. 그 덕분에 게임 개발의 세계를 더 깊이 이해하고 미래의 게임 개발자로서 뛰어난 역량을 갖추게 되었습니다. 이은석 교수님과 함께한 시간은 정말 값진 경험이었습니다. 출처 : chatgpt",""];
+var arr = ["자기소개 : 안녕하세요, 이은석 교수님이지요. 게임 공학 분야의 교육자로, 혁신적이고 독특한 교육 방식을 통해 학생들을 인도합니다.게임 개발의 비밀과 아름다움을 탐험하고, 학생들이 창의적으로 문제를 해결하며 능력을 키우도록 격려합니다. 협업과 팀워크를 강조하며, 학생들과 함께 게임 개발의 새로운 세계를 탐험합니다.출처 : chatgpt",
+"이은석교수님에 대한 소감: 이은석 교수님의 강의와 지도는 마치 마법처럼 환상적입니다. 그의 독특한 교육 방식은 학생들에게 현실적인 경험을 제공하고, 게임 개발의 실무 스킬을 강화합니다. 그 덕분에 게임 개발의 세계를 더 깊이 이해하고 미래의 게임 개발자로서 뛰어난 역량을 갖추게 되었습니다. 이은석 교수님과 함께한 시간은 정말 값진 경험이었습니다.출처 : chatgpt",""];
 function swap(a,b){
     var temp = "";
     if(arr[a] == ""){
